75b412d904706e66b051fce5f56d8253cc352e60
TamirAtia <tamir111994@gmail.com>
2021-01-31 16:21:22 +0200

    Add theme context, Add moadl

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Details from "./Details";
 import { Link, Router } from "@reach/router";
 import SearchParams from "./SearchParams";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const themeHook = useState("darkblue");
   return (
-    <div>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <Router>
-        <SearchParams path="/" />
-        <Details path="/details/:id" />
-      </Router>
-    </div>
+    <ThemeContext.Provider value={themeHook}>
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
